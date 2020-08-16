e2ad937ca91104440b107ede5dbe74df5f7edcb6
Kirsten S <kirsten@Kirstens-MacBook-Air.local>
2020-08-16 19:49:18 -0400

    made outline of projectsRouter (no endpoints yet)

---
+++ projects/projectsRouter.js
@@ -0,0 +1,20 @@
+// import express and helper
+const express = require('express');
+const project = require('../data/helpers/projectModel');
+
+// create router
+const router = express.Router();
+
+// create endpoints
+// get all projects - no body required
+
+// get project by id - id required
+
+// create new project - name & description (str) required, completed boolean optional
+
+// update project - id, name & description (str) required, completed boolean optional
+
+// delete project - id required
+
+// export router
+module.exports = router;
